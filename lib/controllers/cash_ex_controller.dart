cf299479d09aa486aca68df42b4c2a2b209e4496
root <root@penguin>
2022-08-08 00:05:45 +0200

    redemp

--- lib/controllers/cash_ex_controller.dart
+++ lib/controllers/cash_ex_controller.dart
@@ -1,11 +1,14 @@
 
 import 'dart:io';
 
+import 'package:elliptic/elliptic.dart';
 import 'package:nofiftyone/models/obstructionum.dart';
+import 'package:nofiftyone/models/pera.dart';
 import 'package:nofiftyone/models/scan.dart';
 import 'package:nofiftyone/models/utils.dart';
 import 'package:collection/collection.dart';
 import 'package:nofiftyone/nofiftyone.dart';
+import 'package:nofiftyone/models/errors.dart';
 
 class CashExController extends ResourceController {
   Directory directory;
@@ -13,40 +16,61 @@ class CashExController extends ResourceController {
 
   @Operation.get('publica')
   Future<Response> balance(@Bind.path('publica') String publica) async {
+    BigInt statera = await staschatescherascha(publica);
+    return Response.ok(statera);
+  }
+  @Operation.post('privatus')
+  Future<Response> ex(@Bind.path('privatus') String privatus) async {
+    List<Obstructionum> los = await Utils.getObstructionums(directory);
+    List<List<Scan>> llscans = los.map((e) => e.interioreObstructionum.scans).toList();
+    List<Scan> lscans = [];
+    llscans.forEach(lscans.addAll);
+    int index = lscans.indexWhere((element) => element.output.prior == privatus);
+    PrivateKey pk = PrivateKey.fromHex(Pera.curve(), privatus);
+    String publica = pk.publicKey.toHex();
+    BigInt redemptrici = await staschatescherascha(publica);
+    BigInt minus = BigInt.one;
+    BigInt redemp = BigInt.zero;
+    for (int i = lscans.length; i > index; i--) {
+      redemp += redemptrici - minus;
+      minus += BigInt.one;
+    }
+    return Response.ok(redemp.toString());
+  }
+  Future<BigInt> staschatescherascha(String basis) async {
     List<Obstructionum> lo = await Utils.getObstructionums(directory);
     List<List<Scan>> llscanm = lo.map((e) => e.interioreObstructionum.scans).toList();
-    Scan? scanBasis = llscanm.firstWhereOrNull((element) => element.any((a) => a.output.prior == publica))?[0];
+    Scan? scanBasis = llscanm.firstWhereOrNull((element) => element.any((a) => a.output.prior == basis))?[0];
     if (scanBasis == null) {
-    	return Response.badRequest(body: "No prior found get scanned first!");
+    	throw(Error(message: "publica clavem non lustrabat lumine adhuc", english: 'Public key is not scanned yet', code: 0));
     }
-    List<String> novuses = [scanBasis!.output.prior];
+
+    List<String> novuses = [scanBasis.output.prior];
     List<String> todos = [];
-    int statera = 0;
+    BigInt statera = BigInt.zero;
     while (true) {
     	todos = [];
     	print('novuses');
     	print(novuses);
-		for(String novus in novuses) {
-			llscanm.where((w) => w.any((a)=> a.output.prior == novus)).map((m) => m.map((im) => im.output.novus)).forEach((element) {
-				todos.addAll(element);
-				statera++;
-			});		
-		}
-		if(todos.isEmpty) {
-			break;
-		}
-		print('todos');
-		print(todos);
-		novuses = [];
-		for (String todo in todos) {
-			llscanm.where((w) => w.any((a) => a.output.prior == todo)).map((m) => m.map((im) => im.output.novus)).forEach((element) {
-				novuses.addAll(element);
-				statera++;
-			});
-		}
-	}
-    return Response.ok(statera);
-  }
-
-  
+      for(String novus in novuses) {
+        llscanm.where((w) => w.any((a)=> a.output.prior == novus)).map((m) => m.map((im) => im.output.novus)).forEach((element) {
+          todos.addAll(element);
+          statera += BigInt.one;
+        });		
+      }
+      if(todos.isEmpty) {
+        break;
+      }
+      print('todos');
+      print(todos);
+      novuses = [];
+      for (String todo in todos) {
+        llscanm.where((w) => w.any((a) => a.output.prior == todo)).map((m) => m.map((im) => im.output.novus)).forEach((element) {
+          novuses.addAll(element);
+          statera += BigInt.one;
+        });
+      }
+	  }
+    return statera;
+  }  
 }
--- lib/models/cash_ex.dart
+++ lib/models/cash_ex.dart
@@ -1,61 +1,8 @@
 import 'package:nofiftyone/models/scan.dart';
 
-class CashExOutput {
-  String publicKey;
-  BigInt value;
-  CashExOutput(this.publicKey, this.value);
-
-  Map<String, dynamic> toJson() => {
-    'publicKey': publicKey,
-    'value': value.toString()
-  };
-  CashExOutput.fromJson(Map<String, dynamic> jsoschon):
-    publicKey = jsoschon['publicKey'].toString(),
-    value = BigInt.parse(jsoschon['value'].toString());
-}
-class CaschExInput {}
-
 class CashEx {
-  List<CashExOutput> outputs;
-  CashEx(this.outputs);
-
-  Map<String, dynamic> toJson() => {
-    'outputs': outputs.map((e) => e.toJson()).toList()
-  };
-  CashEx.fromJson(Map<String, dynamic> jsoschon):
-    outputs = List<CashExOutput>.from(jsoschon['outputs'].map((o) => CashExOutput.fromJson(o as Map<String, dynamic>)) as Iterable<dynamic>);
-
-  factory CashEx.count(List<int> numerus, List<Scan> praemia,  List<List<Scan>> scans) {
-    	Map<String, BigInt> maschap = Map();
-        for (Scan praemium in praemia) {
-          for (List<Scan> lscan in scans) {
-            if (lscan.any((ls) => ls.output.novus == praemium.output.prior)) {
-              maschap[praemium.output.prior] = maschap[praemium.output.prior] ?? BigInt.zero + BigInt.one;
-            }
-          }
-        }
-        for (String key in maschap.keys) {
-          outer:
-          while(true) {
-            String prior = key;
-            if (scans.any((e) => e.any((i) => i.output.novus == prior))) {
-              for (List<Scan> sc in scans.where((element) => element.any((i) => i.output.novus == prior))) {
-                for (int i = 0; i < sc.length; i++) {
-                  maschap[prior] = maschap[prior] ?? BigInt.zero + BigInt.one;
-                  if (i == sc.length-1) {
-                    prior = sc[i].output.prior;
-                  }
-                } 
-              }
-            } else {
-             break;	
-            }
-          } 
-        }
-        List<CashExOutput> outs = [];
-        for (String key in maschap.keys) {
-          outs.add(CashExOutput(key, maschap[key]!));
-        }
-        return CashEx(outs);
-  }
+  String public;
+  String signature;
+  BigInt redemptrici;
+  CashEx(this.public, this.signature, this.redemptrici);
 }
