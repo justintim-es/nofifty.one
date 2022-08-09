4c44846e8c93189668519d46c2c6b5187c2a4166
root <root@penguin>
2022-08-09 12:48:36 +0200

    rs

--- lib/p2p.dart
+++ lib/p2p.dart
@@ -965,6 +965,7 @@ class P2P {
     for (String socket in sockets) {
       Socket soschock = await Socket.connect(socket.split(':')[0], int.parse(socket.split(':')[1]));
       List<List<String>> hashes = [];
+      sockets.remove(socket);
       // for (int i = 0; i < dir.listSync().length; i++) {
       //   hashes.add([]);
       //   List<String> lines = await Utils.fileAmnis(File('${dir.path}/${Constantes.fileNomen}$i.txt')).toList();
