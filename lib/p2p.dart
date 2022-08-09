9029c081297cc0ea4a3bb286736324fc20e3b4fd
root <root@penguin>
2022-08-09 12:19:03 +0200

    destroy

--- lib/p2p.dart
+++ lib/p2p.dart
@@ -810,6 +810,7 @@ class P2P {
             }
           }
         }
+        client.destroy();
         //  else if (msg.type == 'remove-obstructionum') {
         //     ProbationemP2PMessage pp2pm = ProbationemP2PMessage.fromJson(json.decode(String.fromCharCodes(data).trim()));
         //     await Utils.removeObstructionumsUntilProbationem(pp2pm.probationem, dir);
